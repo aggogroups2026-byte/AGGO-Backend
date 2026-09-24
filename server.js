e30cfd2145aca510c03e06bb7e808467f690c7db
aggogroups2026-byte <aggogroups2026@gmail.com>
2026-09-24 16:45:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const MONGO_URI = "mongodb+srv://aggogroups2026_db_user:<db_password>@cluster0.mzohecd.mongodb.net/?appName=Cluster0";
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
 .then(() => console.log('MongoDB Connected Successfully!'))
